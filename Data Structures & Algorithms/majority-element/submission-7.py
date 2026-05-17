642dae850a0a9516ede40313541839775deb7aa8
Omar Badr <146147057+badromar00@users.noreply.github.com>
2026-05-17 17:12:50 -0500

    Add: majority-element - submission-7

---
+++ Data Structures & Algorithms/majority-element/submission-7.py
@@ -0,0 +1,21 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        maj_count = 1
+        maj_num = nums[0]
+
+        for num in nums[1:]:
+            if maj_count == 0:
+                maj_num = num 
+            maj_count += (1 if maj_num == num else -1)
+        return maj_num
+
+
+"""
+Input: 
+maj_count = -1
+maj_num = 1
+                  i
+nums = [5,5,1,1,1,5,5]
+
+
+"""
